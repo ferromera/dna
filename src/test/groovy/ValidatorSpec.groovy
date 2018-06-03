4d57be9ce9dfca00d544004553b24848e1e3c12b
ferromera <fernando.romera.ferrio@redb.ee>
2018-06-03 20:33:32 -0300

    fix tests

--- src/test/groovy/ValidatorSpec.groovy
+++ src/test/groovy/ValidatorSpec.groovy
@@ -9,25 +9,22 @@ class ValidatorSpec extends Specification{
     DNAValidator validator = new DNAValidator();
 
     def "fails when not square"(){
-        given: DNAChainDTO chain = new DNAChainDTO(
-                dna: [
+        given: String[]  dna= [
                         "12",
                         "1"
                 ]
-        )
-        when: validator.validateSize(chain)
+
+        when: validator.validateSize(dna)
         then: thrown(InvalidDnaException)
     }
 
     def "succeeds when square"(){
-        given: DNAChainDTO chain = new DNAChainDTO(
-                dna: [
+        given: String[] dna= [
                         "125",
                         "142",
                         "142"
                 ]
-        )
-        when: validator.validateSize(chain)
+        when: validator.validateSize(dna)
         then: noExceptionThrown()
     }
 
