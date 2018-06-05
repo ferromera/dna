6862a5dcdc1e31836a883aa43e983f8ee9d40622
ferromera <fernando.romera.ferrio@redb.ee>
2018-06-05 10:26:45 -0300

    change bean in statController

--- src/main/java/ferromera/mutantdetector/controller/StatsController.java
+++ src/main/java/ferromera/mutantdetector/controller/StatsController.java
@@ -3,6 +3,7 @@ package ferromera.mutantdetector.controller;
 
 import ferromera.mutantdetector.dto.StatsDTO;
 import ferromera.mutantdetector.service.StatService;
+import ferromera.mutantdetector.service.impl.StatServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class StatsController {
 
     private StatService statService;
     
-    public StatsController(@Autowired StatService statService) {
+    public StatsController(@Autowired StatServiceImpl statService) {
         this.statService = statService;
     }
     
